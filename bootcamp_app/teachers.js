8e9bd3f67ef11a5b426ad2a854a57ec907955c84
ealhl <yayt.tuk@gmail.com>
2024-02-20 22:18:24 -0800

    fix SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,21 +8,28 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-//Name of Teachers That Assisted
-pool
-  .query(
-    `
+
+// Store all potentially malicious values in an array.
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((row) => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
+  `;
+
+const cohortName = process.argv[2] || "JUL02";
+const value = [`${cohortName}`];
+console.log(value);
+console.log(cohortName);
+//Name of Teachers That Assisted
+pool.query(queryString, value).then((res) => {
+  res.rows.forEach((row) => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   });
+});
+
+
+pool.end();
