8f5763dfc8869f5c5fb997efe67f4a36bc5b6a93
ealhl <yayt.tuk@gmail.com>
2024-02-20 20:51:59 -0800

    add Name of Teachers That Assisted and students

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+//A Pool will manage multiple client connections for us
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "yiva",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+//Name of Teachers That Assisted
+pool
+  .query(
+    `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  });
